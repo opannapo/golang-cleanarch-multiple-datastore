85bc443906d0830df565c5485b69bd4511432ed2
Taufan <taufan.alfazri@gmail.com>
2020-08-23 22:14:13 +0700

    GoLint --> Package app

--- napo-go-firestore/app/router.go
+++ napo-go-firestore/app/router.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-// SetupRoute Router Setup
+//SetupRoute Router Setup
 func SetupRoute(services *services.ServiceInjection) {
 	gin.SetMode(viper.GetString("mode"))
 	router := gin.Default()
