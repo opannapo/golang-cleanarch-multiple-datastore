65e64d3d35126fe5c48760030693e19529d61aa5
Taufan <taufan.alfazri@gmail.com>
2020-09-05 02:44:02 +0700

    GoLint comment for TransactionStruct dynamic return type to handling transaction commit & roolback for each database type

--- napo-go-firestore/app/v1/repository/transaction_struct.go
+++ napo-go-firestore/app/v1/repository/transaction_struct.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+//TransactionStruct dynamic return type to handling transaction commit & roolback for each database type
 type TransactionStruct struct {
 	GormTX  *gorm.DB
 	RedisTX *redis.Tx
